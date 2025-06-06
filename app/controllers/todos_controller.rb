653ea02ecb815cf0571a5126dbf7f3ef2e323352
shindeshubhamm <shubhamshinde.3151@gmail.com>
2025-06-06 14:52:05 +0530

    add todos controller

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,47 @@
+class TodosController < ApplicationController
+  before_action :set_todo, only: [ :show, :update, :destroy ]
+
+  def index
+    @todos = Todo.all
+    render json: @todos
+  end
+
+  def show
+    render json: @todo
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+
+    if @todo.save
+      render json: @todo, status: :created
+    else
+      render json: { errors: @todo.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @todo.update(todo_params)
+      render json: @todo
+    else
+      render json: { errors: @todo.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @todo.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Todo not found" }, status: :not_found
+  end
+
+  def todo_params
+    params.require(:todo).permit(:title, :description, :status)
+  end
+end
